255236810b71065926631596d3684cfd85dea87f
221RDB024 <125682199+BR4INFREEZE@users.noreply.github.com>
2023-02-19 21:51:53 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,44 @@
-# python3
-
+import os
 from collections import namedtuple
 
 Bracket = namedtuple("Bracket", ["char", "position"])
 
 
-def are_matching(left, right):
+def check(left, right):
     return (left + right) in ["()", "[]", "{}"]
 
 
 def find_mismatch(text):
-    opening_brackets_stack = []
+    stack = []
     for i, next in enumerate(text):
         if next in "([{":
-            # Process opening bracket, write your code here
-            pass
-
+            stack.append(Bracket(next, i+1))
         if next in ")]}":
-            # Process closing bracket, write your code here
-            pass
+            if not stack or not check(stack[-1].char, next):
+                return i+1
+            stack.pop()
+    if stack:
+        return stack[0].position
+    return "Success"
 
 
 def main():
-    text = input()
-    mismatch = find_mismatch(text)
-    # Printing answer, write your code here
+    first_input = input()
+    if first_input.__contains__('I'):
+        text_line = input()
+    elif first_input.__contains__('F'):
+        file_name = input()
+        if os.path.exists(file_name):
+            with open(file_name) as file:
+                text_line = file.read()
+        else:
+            print("INPUT-OUTPUT ERROR")
+            return
+    else:
+        print("INPUT-OUTPUT ERROR")
+        return
+    output = find_mismatch(text_line)
+    print(output)
 
 
 if __name__ == "__main__":

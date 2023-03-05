860b81d1b3eb9dd83cd7f3b0a7f8178cfc5c665c
221RDB024 <125682199+BR4INFREEZE@users.noreply.github.com>
2023-03-05 21:47:35 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,44 +1,59 @@
+import sys
+import threading
 import os
-from collections import namedtuple
+import numpy as np
 
-Bracket = namedtuple("Bracket", ["char", "position"])
-
-
-def check(left, right):
-    return (left + right) in ["()", "[]", "{}"]
+c = 0
 
 
-def find_mismatch(text):
-    stack = []
-    for i, next in enumerate(text):
-        if next in "([{":
-            stack.append(Bracket(next, i+1))
-        if next in ")]}":
-            if not stack or not check(stack[-1].char, next):
-                return i+1
-            stack.pop()
-    if stack:
-        return stack[0].position
-    return "Success"
+def aray(size, numbers):
 
+    splitted = numbers.split()
+    arr1 = np.zeros(int(size))
+    i, k, start = 0, 1, 0
+    for a in range(arr1.size):
+        arr1[i] = int(splitted[i])
+        if arr1[i] == -1:
+            start = i
+        i += 1
+    check(k, start, arr1)
+
+
+def check(k, start, arr1):
+
+    global c
+    for i in range(arr1.size):
+        if arr1[i] == start:
+            move = i
+            k += 1
+            if c < k:
+                c = k
+            k = check(k, move, arr1)
+    k -= 1
+    return k
 
 def main():
+
     first_input = input()
     if first_input.__contains__('I'):
-        text_line = input()
+        second_input = input()
+        third_input = input()
+        aray(second_input, third_input)
     elif first_input.__contains__('F'):
         file_name = input()
         if os.path.exists(file_name):
-            with open(file_name) as file:
-                text_line = file.read()
+            file = os.path.join(os.getcwd(), 'test', file_name)
+            with open(file, 'r') as f:
+                second_input = f.readline()
+                third_input = str(f.readline())
+                aray(second_input, third_input)
         else:
             print("INPUT-OUTPUT ERROR")
             return
     else:
         print("INPUT-OUTPUT ERROR")
         return
-    output = find_mismatch(text_line)
-    print(output)
+    print(c)
 
 
 if __name__ == "__main__":
